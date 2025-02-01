adbb227b032cd82fbb64d78a3c630f60de9532ad
Md-Manir-hossain <mdmanirhossainb150@gmail.com>
2025-02-01 14:00:24 +0600

    conditional statement

---
+++ script.js
@@ -0,0 +1,56 @@
+// ======General Math for Practice=====
+console.log ("Total Amount or Purchase = 500 Taka.")
+console.log ('Eggs Cost = 15*5') 
+let EggsValue = 15
+let Eggslimit = 5
+let EggsTotal = EggsValue*Eggslimit;
+console.log(EggsTotal);
+console.log ('======================') 
+
+console.log ('Kitkat Cost = 80*2') 
+let KitkatValue = 80
+let Kitkatlimit = 2
+let KitkatTotal = KitkatValue*Kitkatlimit;
+console.log(KitkatTotal);
+console.log ('======================') 
+
+console.log ('Eggs Value Total =    75') 
+console.log ('Kitkat Value Total = 160') 
+console.log ('--------------------------') 
+let total1 = 75
+let total2 = 160
+let totalCost = total1+total2;
+console.log(totalCost);
+console.log ('======================') 
+
+console.log ('Value Remaining = 500-255') 
+console.log ('--------------------------') 
+let Remaining1 = 500
+let Remaining2 = 235
+let Remaining = Remaining1-Remaining2;
+console.log(Remaining);
+console.log ('======================') 
+
+
+console.log ("======== Javascript Conditional Statement ========")
+
+let rahim = "walk";
+let karim = "bike";
+let bablu = "car";
+
+if (rahim == "cycle") {
+    console.log ("I will go to Field/It can be plus, minus or others")
+}
+
+else if (karim == "walk") {
+    console.log ("I will go the park/It can be plus, minus or others")
+}
+
+else if (bablu == "rickshaw") {
+    console.log ("I will go to long drive/It can be plus, minus or others")
+}
+
+else {
+    console.log ("I will enjoy movies in my room/It can be plus, minus or others")
+}
+
